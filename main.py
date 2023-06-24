15594d06c9451d229fba198e2053544adb722c1c
devabacus <devabacusnrf@gmail.com>
2023-06-24 11:42:30 +0530

    1

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ print("hello my friend")
 
 @dp.message_handler(commands = ['start'])
 async def start(msg:types.Message):
-    markup = types.ReplyKeyboardMarkup()
-    markup.add(types.KeyboardButton('Открыть веб-страницу', web_app=WebAppInfo(url='https://devabacus.github.io/telegram_bots_test/')))
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Открыть веб-страницу', web_app=WebAppInfo(url='https://devabacus.github.io/telegram_bots_test/')))
     await msg.answer("Привет пацанчик", reply_markup=markup)
     
 executor.start_polling(dp)

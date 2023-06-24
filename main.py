dfca4e0e3d6a0bb0cb62f5d5114347a70982bd9f
devabacus <devabacusnrf@gmail.com>
2023-06-24 12:06:00 +0530

    1

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from aiogram.types.web_app_info import WebAppInfo
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot)
 
-print("hello my friend")
-
 @dp.message_handler(commands = ['start'])
 async def start(msg:types.Message):
     markup = types.ReplyKeyboardMarkup()
